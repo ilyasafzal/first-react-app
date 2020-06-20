4700ebb0dcc2ff45ee62bec0f09f53fb9da207a8
ILYAS_AFZAL <muhammadilyazafzal@gmail.com>
2020-06-20 08:12:12 +0500

    first app intro sbjects 11

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-
+import Subjects from './subjects.js';
 function App() {
   return (
     <div> 
-                 HELLO  WORLD : I AM ILYAS AFZAL :    
+    <subjects />
     </div>
   );
 }
---
+++ src/subjects.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function subjects() {
+ reture (  
+     <div> 
+     <h1>IF YOU TAKE PRE-ENGEENRING FIELD</h1>
+     <h1>There are 6 subjects.which you are study in first year </h1> 
+     <h1>1st subject is Chemistry </h1>
+     <h1>2nd subject is Physics</h1>
+     <h1>3rd subjest is Mathematics </h1>
+     <h1>4th subject is English </h1>
+     <h1>5th subject is Urdu</h1>
+     <h1>6th subjest is Islamiat </h1>
+     </div>
+
+ );
+}
+export default subjects;
